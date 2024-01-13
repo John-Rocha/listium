1cf20b3f2c49789751359d1fc996484be42b8797
Johnathan Rocha <johnathanrocha@gmail.com>
2024-01-13 13:38:37 -0300

    feat: firebase configurado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:listium/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
