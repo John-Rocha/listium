454b868d30ef3b7c11b79b1ec1ad8fd7bb184f00
Johnathan Rocha <johnathanrocha@gmail.com>
2024-01-13 16:00:49 -0300

    feat: add data de criação

--- lib/models/listium.dart
+++ lib/models/listium.dart
@@ -1,15 +1,19 @@
 class Listium {
-  String id;
-  String name;
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
   Listium({
     required this.id,
     required this.name,
+    required this.createdAt,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'id': id,
       'name': name,
+      'createdAt': createdAt.toUtc().toIso8601String(),
     };
   }
 
@@ -17,6 +21,7 @@ class Listium {
     return Listium(
       id: map['id'],
       name: map['name'],
+      createdAt: DateTime.parse(map['createdAt']).toLocal(),
     );
   }
 }
